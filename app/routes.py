36577ce61a068df58af763c35cdc467d081a2d9d
manojd <manoj.ducher@gmail.com>
2018-02-08 19:52:17 +0100

    created base.html from django blog

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,11 @@
 ''' routes.py file '''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('base.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
